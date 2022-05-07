1b2a9c5c171ccdb72adaaf8564957861568a5ae7
lian <test@lian.com>
2022-05-07 22:38:51 +0800

    brach commit

--- src/main/java/com/git/test/test.java
+++ src/main/java/com/git/test/test.java
@@ -3,5 +3,7 @@ package com.git.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello222");
+        System.out.println("hello");
     }
 }
